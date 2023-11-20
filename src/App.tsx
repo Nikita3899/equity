88aac403d6d6d98baf63882880f0ced00f98f02c
Nikita Sharma <nikitasharma3899@gmail.com>
2023-11-21 05:25:17 +0530

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import  style from './app.module.scss';
+import React from 'react';
+
+function App() {
+  return (
+    <div className={style["App"]}>
+   
+    </div>
+  );
+}
+
+export default App;
